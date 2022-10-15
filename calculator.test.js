b69f73d0c42f3d68ec315e99c84186eb7737fce0
uttam rawat <uttamrawat146>
2022-10-15 23:09:01 +0530

    Unit tesing the calculator module using jest

---
+++ calculator.test.js
@@ -0,0 +1,20 @@
+const mathOperation = require('./calculator');
+
+describe('Calculator tests',()=>{
+    test("addition of two numbers",()=>{
+        var results = mathOperation.add(1,2);
+        expect(results).toBe(3);
+    })
+    test('subtract of two numbers',()=>{
+        var results = mathOperation.subtract(3,5);
+        expect(results).toBe(-2);
+    })
+    test('multiply of two numbers',()=>{
+        var results = mathOperation.multiply(10,4);
+        expect(results).toBe(40);
+    })
+    test('divide of two numbers',()=>{
+        var results = mathOperation.divide(45,5);
+        expect(results).toBe(9);
+    })
+})
